b6ab33f954a0d1063e003c59fe133a3f6f133b69
KalalChandu <kalalshannu19@gmail.com>
2024-09-29 00:06:26 +0530

    refactoring: the code

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,27 @@
 import { useState, useEffect } from "react";
-import "./App.css";
 import Header from "./Components/Header";
 import About from "./components/about";
 import Resume from "./components/resume";
 import Portfolio from "./components/portfolio";
 import Contact from "./components/contact";
 import Footer from "./components/footer";
+import { resumeData } from "./data/resume-data";
 
 const App = () => {
-  const [resumeData, setResumeData] = useState({});
+  const [data, setData] = useState({});
 
   useEffect(() => {
-    fetch("/resumeData.json")
-      .then((response) => response.json())
-      .then((data) => setResumeData(data))
-      .catch((error) => {
-        console.log(error);
-        alert(error);
-      });
+    setData(resumeData); // Set the imported resumeData
   }, []);
 
   return (
     <div className="App">
-      <Header data={resumeData.main} />
-      <About data={resumeData.main} />
-      <Resume data={resumeData.resume} />
-      <Portfolio data={resumeData.portfolio} />
-      <Contact data={resumeData.main} />
-      <Footer data={resumeData.main} />
+      <Header data={data.main} />
+      <About data={data.main} />
+      <Resume data={data.resume} />
+      <Portfolio data={data.portfolio} />
+      <Contact data={data.main} />
+      <Footer data={data.main} />
     </div>
   );
 };
